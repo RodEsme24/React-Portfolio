7d6b40f0b9e94bd8ce32e25a0b206a4d04fec08a
esme rodriguez <rodriguezesme24@gmail.com>
2020-07-23 10:17:27 -0700

    Updates

---
+++ precache-manifest.e26f1a549667d0a4319163a688a55991.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5329bbbf33e154ebf54d0f31eb3c3f0a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2e23d08632cfc2d1eac4",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7265a29afa84d95731ff",
+    "url": "/react-portfolio/static/css/main.117a4535.chunk.css"
+  },
+  {
+    "revision": "2e23d08632cfc2d1eac4",
+    "url": "/react-portfolio/static/js/2.400328ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.400328ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7265a29afa84d95731ff",
+    "url": "/react-portfolio/static/js/main.70f43ef8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6c167ec720dcf600ab402e4a6bfe91f3",
+    "url": "/react-portfolio/static/media/esme.6c167ec7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
